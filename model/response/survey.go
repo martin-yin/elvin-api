018ecfe67cfec88aff1d4eef2e07879892f58f0d
Martin <martin-yin@foxmail.com>
2021-04-28 21:14:37 +0800

    commit

--- model/response/survey.go
+++ model/response/survey.go
@@ -1,11 +1,11 @@
 package response
 
 type SurveyStatisticsResponse struct {
-	Pv       int    `json:"pv"`
-	Uv       int    `json:"uv"`
-	Ip       string `json:"ip"`
-	Resources int `json:"resources"`
-	JsError int `json:"js_error"`
+	Pv        int    `json:"pv"`
+	Uv        int    `json:"uv"`
+	Ip        string `json:"ip"`
+	Resources int    `json:"resources"`
+	JsError   int    `json:"js_error"`
 }
 
 type LoadPageList struct {
@@ -13,8 +13,7 @@ type LoadPageList struct {
 	TimeKey  int `json:"time_key"`
 }
 
-
 type SurveyPerformancesResponse struct {
 	LoadPage float64 `json:"load_page"`
-	TimeKey  string `json:"time_key"`
+	TimeKey  string  `json:"time_key"`
 }
